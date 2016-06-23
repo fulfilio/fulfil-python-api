74f05f9b30fe7eb9813e2f7d0a8fe2dc232056a6
Sharoon Thomas <sharoon.thomas@fulfil.io>
2016-06-23 09:02:10 +0530

    Add pytest based tests

--- setup.py
+++ setup.py
@@ -47,13 +47,12 @@ setup(
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
     ],
-    test_suite='tests',
-    tests_require=test_requirements
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
 )
