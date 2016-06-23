140e41479bbebf2afeebbc2328f7af26413b5dc1
Sharoon Thomas <sharoon.thomas@fulfil.io>
2016-06-23 09:23:58 +0530

    Unify quotes

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
