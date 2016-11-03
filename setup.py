a069da312f973411ae39101a61e2fae523a15b2f
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2016-11-04 03:49:10 +0530

    Released version 0.6.11

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='fulfil_client',
-    version='0.6.10',
+    version='0.6.11',
     description="Fulfil REST API Client in Python",
     long_description=readme + '\n\n' + history,
     author="Fulfil.IO Inc.",
