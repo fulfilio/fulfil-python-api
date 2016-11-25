51eb645c3b84cf8e0ea4d7f530b403e530819014
Sharoon Thomas <sharoon.thomas@fulfil.io>
2016-11-25 14:28:48 -0500

    Add a search_read iterator

--- tests/test_fulfil_client.py
+++ tests/test_fulfil_client.py
@@ -19,6 +19,15 @@ def test_find(client):
     assert ir_models[0]['rec_name']
 
 
+def test_search_read_all(client):
+    IRModel = client.model('ir.model')
+    ir_models = list(IRModel.search_read_all([], None, ['rec_name'], 5))
+    assert len(ir_models) == IRModel.search_count([])
+    assert len(ir_models) > 10
+    assert ir_models[0]['id']
+    assert ir_models[0]['rec_name']
+
+
 def test_find_no_filter(client):
     IRModel = client.model('ir.model')
     ir_models = IRModel.find()
