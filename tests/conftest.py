236c7f410d98248d59e2b475acdf8c7907412e9e
Sharoon Thomas <sharoon.thomas@fulfil.io>
2019-02-04 16:21:15 -0500

    Add the ability to mock api for tests
    This implements a convenient mocking api for testing Fulfil mock
    api calls. Does not add mock as a dependency because it's part of stdlib
    on python 3 and it's a contrib module where the user writing tests should
    install mock if needed.
    
    [FP-5166]

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 """Defines fixtures available to all tests."""
-import os
-
 import pytest
 import redis
 
@@ -11,13 +9,13 @@ from fulfil_client.model import model_base
 
 @pytest.fixture
 def client():
-    return Client('demo', os.environ['FULFIL_API_KEY'])
+    return Client('demo', 'test-api-key')
 
 
 @pytest.fixture
 def oauth_client():
     return Client(
-        'demo', auth=BearerAuth(os.environ['FULFIL_OAUTH_TOKEN'])
+        'demo', auth=BearerAuth('user-test-token')
     )
 
 
