6db96f8d1d658cd81e27fe73e027eac7c2a4b374
Sharoon Thomas <sharoon.thomas@fulfil.io>
2019-03-06 14:39:44 -0500

    Bump version: 0.13.0 → 0.13.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ requirements = [
 
 setup(
     name='fulfil_client',
-    version='0.13.0',
+    version='0.13.1',
     description="Fulfil REST API Client in Python",
     long_description=readme + '\n\n' + history,
     author="Fulfil.IO Inc.",
